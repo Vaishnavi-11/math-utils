d25133b5ec501db6d505a4106e0e6bc2fc61db37
Vaishnavi N <62497995+Vaishnavi-11@users.noreply.github.com>
2026-03-29 16:08:33 +0530

    Add unit tests for add and multiply functions

---
+++ test_math_utils.py
@@ -0,0 +1,10 @@
+from math_utils import add, multiply
+
+def test_add():
+  assert add(2,3) == 5
+
+def test_add_negative():
+  assert add(-1,1) == 0
+
+def test_multiply():
+  assert multiply(3,4) == 12
